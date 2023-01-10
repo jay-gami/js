f6136dae39a613730df9fa336e2418700eefab7d
jay-gami <jay.gami@softices.com>
2023-01-10 15:30:21 +0530

    some chnaez

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var Age=10;
+var Age=5   ;
 
 // console.log(`${ Age>=20 ? "i am adult" : "i am a boy"}`)
 
 // console.log(`${"i am a"+ Age}`
 // 
-// console.log(`${30+Age +"   years old"}`);
+console.log(`${`${Age * 12} years old`}`);
