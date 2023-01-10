94302fb8a90bf5b6758b300e11fcbf22db141347
jay-gami <jay.gami@softices.com>
2023-01-10 15:16:36 +0530

    new

---
+++ func.js
@@ -0,0 +1,19 @@
+// function calcAge(currentYear,BirthYear){
+//     return  currentYear-BirthYear;
+
+// }
+
+
+
+// arrow function
+// const calcAge = (currentYear,BirthYear) => currentYear-BirthYear
+
+// console.log(calcAge(2022,2002))
+
+//exprssion
+
+const calcAge = (currentYear, BirthYear) => {
+    console.log(currentYear - BirthYear);
+
+} 
+calcAge(2022,2000);
---
+++ main.js
@@ -0,0 +1,7 @@
+var Age=10;
+
+// console.log(`${ Age>=20 ? "i am adult" : "i am a boy"}`)
+
+// console.log(`${"i am a"+ Age}`
+// 
+// console.log(`${30+Age +"   years old"}`);
